4ce5603030ab446bfc69f62c6b45a723c56be3c9
Ali M <alimousavi-01@outlook.com>
2022-02-03 21:05:24 +0330

    Created Product Entity

---
+++ src/Catalog/Catalog.Api/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.Api.Entities;
+
+public class Product
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    [BsonElement("Title")]
+    public string Title { get; set; }
+
+    [BsonElement("Category")]
+    public string Category { get; set; }
+
+    [BsonElement("Summary")]
+    public string Summary { get; set; }
+
+    [BsonElement("Description")]
+    public string Description { get; set; }
+
+    [BsonElement("ImagePath")]
+    public string ImagePath { get; set; }
+
+    [BsonElement("Price")]
+    public decimal Price { get; set; }
+}
+
