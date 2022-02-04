d9656831059136e98f93abd754f91d49dea4361e
Ali M <alimousavi-01@outlook.com>
2022-02-04 16:59:21 +0330

    Implemented ProductRepository

---
+++ src/Catalog/Catalog.Api/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace Catalog.Api.Repositories.Interfaces;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetAllProducts();
+
+    Task<Product> GetProductById(string id);
+
+    Task<IEnumerable<Product>> GetProductByName(string title);
+
+    Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
+
+    Task CreateProduct(Product product);
+
+    Task<bool> UpdateProduct(Product product);
+
+    Task<bool> DeleteProduct(string id);
+}
