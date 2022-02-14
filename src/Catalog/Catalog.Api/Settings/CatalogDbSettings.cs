358f238f0daf5b1a82e7b6faf11f84c8ae3a0964
Ali M <alimousavi-01@outlook.com>
2022-02-14 20:19:19 +0330

    Added mongo-express

--- src/Catalog/Catalog.Api/Settings/CatalogDbSettings.cs
+++ src/Catalog/Catalog.Api/Settings/CatalogDbSettings.cs
@@ -2,18 +2,45 @@
 
 public interface ICatalogDbSettings
 {
-    string ConnectionString { get; set; }
+    string ConnectionString { get; }
 
     string DbName { get; set; }
+
+    string Host { get; set; }
+
+    int Port { get; set; }
+
+    string User { get; set; }
+
+    string Password { get; set; }
 
     string CollectionName { get; set; }
 }
 
 public class CatalogDbSettings : ICatalogDbSettings
 {
-    public string ConnectionString { get; set; }
+    public string ConnectionString
+    {
+        get
+        {
+            if (string.IsNullOrEmpty(User) || string.IsNullOrEmpty(Password))
+                return $@"mongodb://{Host}:{Port}";
+
+            return $@"mongodb://{User}:{Password}@{Host}:{Port}";
+        }
+
+    }
 
     public string DbName { get; set; }
 
+    public string Host { get; set; }
+
+    public int Port { get; set; }
+
+    public string User { get; set; }
+
+    public string Password { get; set; }
+
     public string CollectionName { get; set; }
+
 }
