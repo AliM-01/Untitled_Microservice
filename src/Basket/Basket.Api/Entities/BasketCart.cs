6efe472581bd02d4ff4f3120aa86a14bdd6f6e0a
Ali M <alimousavi-01@outlook.com>
2022-02-16 15:49:06 +0330

    Created Basket Entities

---
+++ src/Basket/Basket.Api/Entities/BasketCart.cs
@@ -0,0 +1,32 @@
+﻿namespace Basket.Api.Entities;
+
+public class BasketCart
+{
+    public string UserName { get; set; }
+
+    public List<BasketCartItem> Items { get; set; } = new List<BasketCartItem>();
+
+    public BasketCart()
+    {
+    }
+
+    public BasketCart(string username)
+    {
+        UserName = username;
+    }
+
+    public decimal TotalPrice
+    {
+        get
+        {
+            decimal total = 0;
+
+            foreach (var item in Items)
+            {
+                total += item.Price * item.Quantity;
+            }
+
+            return total;
+        }
+    }
+}
---
+++ src/Basket/Basket.Api/Entities/BasketCheckout.cs
@@ -0,0 +1,24 @@
+﻿namespace Basket.Api.Entities;
+
+public class BasketCheckout
+{
+    public string UserName { get; set; }
+
+    public decimal TotalPrice { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Email { get; set; }
+
+    public string CardName { get; set; }
+
+    public string CardNumber { get; set; }
+
+    public string CardExpr { get; set; }
+
+    public string CVV { get; set; }
+
+    public int PaymentMethod { get; set; }
+}
