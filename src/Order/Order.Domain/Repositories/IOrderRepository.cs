38a1129c08cb5723f49ee128d36770a1c6f5aece
Ali M <alimousavi-01@outlook.com>
2022-05-02 21:59:11 +0430

    Created IOrderRepository

---
+++ src/Order/Order.Domain/Repositories/IOrderRepository.cs
@@ -0,0 +1,7 @@
+﻿using Order.Domain.Repositories.Base;
+
+namespace Order.Domain.Repositories;
+public interface IOrderRepository : IRepository<Entities.Order>
+{
+    Task<IEnumerable<Entities.Order>> GetOrderByUserName(string userName);
+}
