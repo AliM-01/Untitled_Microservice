b7feb8f65e9f820fd385b3fd9508f50728c9f4fc
Ali M <alimousavi-01@outlook.com>
2022-05-03 13:00:02 +0430

    Implemented CreateAndSeedDatabaseAsync in Order.Api

--- src/Order/Order.Api/Program.cs
+++ src/Order/Order.Api/Program.cs
@@ -1,8 +1,15 @@
+using Order.Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Host
+    .ConfigureHostOptions(o => o.ShutdownTimeout = TimeSpan.FromSeconds(30))
+    .Build()
+    .CreateAndSeedDatabaseAsync();
 
 var app = builder.Build();
 
