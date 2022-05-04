83562e46d78d1e3b11756f08a068cdd4d8e25555
Ali M <alimousavi-01@outlook.com>
2022-05-04 13:04:57 +0430

    Configured Sql Server Db Connection

--- src/Order/Order.Api/Program.cs
+++ src/Order/Order.Api/Program.cs
@@ -1,12 +1,17 @@
+using Microsoft.EntityFrameworkCore;
 using Order.Api.Extensions;
+using Order.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<OrderDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("OrderDb")), ServiceLifetime.Singleton);
+
+builder.Services.CreateAndSeedDatabaseAsync();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.CreateAndSeedDatabaseAsync();
 
 var app = builder.Build();
 
