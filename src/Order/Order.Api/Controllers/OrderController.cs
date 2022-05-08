5e59bf38772ec3f6b147f1b770292c92c5c6e176
Ali M <alimousavi-01@outlook.com>
2022-05-08 20:39:52 +0430

    Created OrderController

---
+++ src/Order/Order.Api/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Order.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class OrderController : ControllerBase
+{
+    #region ctor
+
+    private readonly IMediator _mediator;
+
+    public OrderController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    #endregion
+}
