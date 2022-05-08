4b21b57fc6b827460fd326f31c627336be50cd7a
Ali M <alimousavi-01@outlook.com>
2022-05-08 20:58:17 +0430

    Order.Api Program.cs Config Added

--- src/Order/Order.Api/Program.cs
+++ src/Order/Order.Api/Program.cs
@@ -1,6 +1,13 @@
+using System.Reflection;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Order.Api.Extensions;
+using Order.Application;
+using Order.Domain.Repositories;
+using Order.Domain.Repositories.Base;
 using Order.Infrastructure.Data;
+using Order.Infrastructure.Repositories;
+using Order.Infrastructure.Repositories.Base;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +15,14 @@ builder.Services.AddDbContext<OrderDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("OrderDb")), ServiceLifetime.Singleton);
 
 builder.Services.CreateAndSeedDatabaseAsync();
+
+builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.AddMediatR(typeof(IOrderApplicationAssemblyMarker).GetTypeInfo().Assembly);
+
+builder.Services.AddTransient<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+builder.Services.AddScoped(typeof(IOrderRepository), typeof(OrderRepository));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
