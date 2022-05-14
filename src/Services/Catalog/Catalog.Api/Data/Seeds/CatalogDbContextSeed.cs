e0a99f5315612145aa15e617b5ef8ee226fb991d
Ali M <alimousavi-01@outlook.com>
2022-05-14 20:00:26 +0430

    Added Polly Retry to Catalog Seed

--- src/Services/Catalog/Catalog.Api/Data/Seeds/CatalogDbContextSeed.cs
+++ src/Services/Catalog/Catalog.Api/Data/Seeds/CatalogDbContextSeed.cs
@@ -1,45 +1,57 @@
-﻿namespace Catalog.Api.Data.Seeds
+﻿using Polly;
+
+namespace Catalog.Api.Data.Seeds;
+
+public class CatalogDbContextSeed
 {
-    public static class CatalogDbContextSeed
+    private const int MAX_RETRIES = 3;
+
+    public static void SeedData(IMongoCollection<Product> products)
     {
-        public static void SeedData(IMongoCollection<Product> products)
+        var retryPolicy = Policy.Handle<Exception>()
+                    .WaitAndRetry(
+                       retryCount: MAX_RETRIES,
+                       sleepDurationProvider: times => TimeSpan.FromSeconds(times));
+
+        retryPolicy.Execute(() =>
         {
             bool existsProduct = products.Find(_ => true).Any();
 
-            if (!existsProduct)
+            if (existsProduct)
+                return;
+
+            List<Product> productToAdd = new()
             {
-                List<Product> productToAdd = new()
+                new Product
                 {
-                    new Product
-                    {
-                        Title = "IPHONE 13",
-                        Price = 1000,
-                        Category = "Smart Phone",
-                        Summary = "Lorem ipsum dolor sit amet, consectetur adipiscing",
-                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-                        ImagePath = "iphone_13.jpg"
-                    },
-                    new Product
-                    {
-                        Title = "IPHONE 12",
-                        Price = 999,
-                        Category = "Smart Phone",
-                        Summary = "Lorem ipsum dolor sit amet, consectetur adipiscing",
-                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-                        ImagePath = "iphone_12.jpg"
-                    },
-                    new Product
-                    {
-                        Title = "IPHONE 11",
-                        Price = 899,
-                        Category = "Smart Phone",
-                        Summary = "Lorem ipsum dolor sit amet, consectetur adipiscing",
-                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-                        ImagePath = "iphone_11.jpg"
-                    }
-                };
-                products.InsertManyAsync(productToAdd);
-            }
-        }
+                    Title = "IPHONE 13",
+                    Price = 1000,
+                    Category = "Smart Phone",
+                    Summary = "Lorem ipsum dolor sit amet, consectetur adipiscing",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+                    ImagePath = "iphone_13.jpg"
+                },
+                new Product
+                {
+                    Title = "IPHONE 12",
+                    Price = 999,
+                    Category = "Smart Phone",
+                    Summary = "Lorem ipsum dolor sit amet, consectetur adipiscing",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+                    ImagePath = "iphone_12.jpg"
+                },
+                new Product
+                {
+                    Title = "IPHONE 11",
+                    Price = 899,
+                    Category = "Smart Phone",
+                    Summary = "Lorem ipsum dolor sit amet, consectetur adipiscing",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+                    ImagePath = "iphone_11.jpg"
+                }
+            };
+            products.InsertManyAsync(productToAdd);
+
+        });
     }
 }
