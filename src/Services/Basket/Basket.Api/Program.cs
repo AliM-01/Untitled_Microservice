c08ecc14dc8b62f5deffd795b25ada82e04de231
Ali M <alimousavi-01@outlook.com>
2022-05-14 19:34:31 +0430

    Added Health Check to Basket Api

--- src/Services/Basket/Basket.Api/Program.cs
+++ src/Services/Basket/Basket.Api/Program.cs
@@ -1,58 +1,10 @@
-using Basket.Api.Data;
-using Basket.Api.Data.Interfaces;
-using Basket.Api.Data.Mappings;
-using Basket.Api.Repositories;
-using Basket.Api.Repositories.Interfaces;
-using EventBusRabbitMQ;
-using EventBusRabbitMQ.Producer;
-using RabbitMQ.Client;
-using StackExchange.Redis;
+using Basket.Api;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
-#region configure services
-
-builder.Services.AddSingleton<ConnectionMultiplexer>(sp =>
-{
-    var config = ConfigurationOptions.Parse(builder.Configuration.GetConnectionString("Redis"), true);
-
-    return ConnectionMultiplexer.Connect(config);
-});
-
-builder.Services.AddTransient<IBasketContext, BasketContext>();
-builder.Services.AddTransient<IBasketRepository, BasketRepository>();
-
-builder.Services.AddAutoMapper(typeof(BasketMappingProfile));
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-#region RabbitMQ
-
-builder.Services.AddSingleton<IRabbitMQConnection>(sp =>
-{
-    var factory = new ConnectionFactory()
-    {
-        HostName = builder.Configuration["EventBus:HostName"]
-    };
-
-    if (!string.IsNullOrEmpty(builder.Configuration["EventBus:UserName"]))
-    {
-        factory.UserName = builder.Configuration["EventBus:UserName"];
-        factory.Password = builder.Configuration["EventBus:Password"];
-    }
-
-    return new RabbitMQConnection(factory);
-});
-
-builder.Services.AddSingleton<EventBusProducer>();
-
-#endregion
-
-#endregion
-
-#region configure
+builder.Services.RegisterServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -64,7 +16,10 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHealthChecks("/hc", new HealthCheckOptions()
+{
+    Predicate = _ => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 app.Run();
-
-#endregion
---
+++ src/Services/Basket/Basket.Api/ServiceRegister.cs
@@ -0,0 +1,61 @@
+﻿using Basket.Api.Data;
+using Basket.Api.Data.Interfaces;
+using Basket.Api.Data.Mappings;
+using Basket.Api.Repositories;
+using Basket.Api.Repositories.Interfaces;
+using EventBusRabbitMQ;
+using EventBusRabbitMQ.Producer;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using RabbitMQ.Client;
+using StackExchange.Redis;
+
+namespace Basket.Api;
+
+public static class ServiceRegister
+{
+    public static IServiceCollection RegisterServices(this IServiceCollection services, IConfiguration config)
+    {
+        string connectionString = config.GetConnectionString("Redis");
+
+        services.AddSingleton<ConnectionMultiplexer>(sp =>
+        {
+            var config = ConfigurationOptions.Parse(connectionString, true);
+
+            return ConnectionMultiplexer.Connect(config);
+        });
+
+        services.AddTransient<IBasketContext, BasketContext>();
+        services.AddTransient<IBasketRepository, BasketRepository>();
+
+        services.AddAutoMapper(typeof(BasketMappingProfile));
+
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddSingleton<IRabbitMQConnection>(sp =>
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = config["EventBus:HostName"]
+            };
+
+            if (!string.IsNullOrEmpty(config["EventBus:UserName"]))
+            {
+                factory.UserName = config["EventBus:UserName"];
+                factory.Password = config["EventBus:Password"];
+            }
+
+            return new RabbitMQConnection(factory);
+        });
+
+        services.AddSingleton<EventBusProducer>();
+
+        services.AddHealthChecks()
+                    .AddRedis(connectionString,
+                              "Redis Health",
+                              HealthStatus.Degraded);
+
+        return services;
+    }
+}
