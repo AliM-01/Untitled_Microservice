a9e830784c1a7eed1746225bfa53b1bd28222c22
Ali M <alimousavi-01@outlook.com>
2022-05-14 19:24:08 +0430

    Added Health Check to Catalog Api

--- src/Services/Catalog/Catalog.Api/Program.cs
+++ src/Services/Catalog/Catalog.Api/Program.cs
@@ -1,19 +1,10 @@
-using Catalog.Api.Data;
-using Catalog.Api.Data.Interfaces;
-using Catalog.Api.Repositories;
-using Catalog.Api.Repositories.Interfaces;
-using Catalog.Api.Settings;
+using Catalog.Api;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.Configure<CatalogDbSettings>(builder.Configuration.GetSection("CatalogDbSettings"));
-
-builder.Services.AddTransient<ICatalogDbContext, CatalogDbContext>();
-builder.Services.AddTransient<IProductRepository, ProductRepository>();
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.RegisterServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -25,5 +16,10 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHealthChecks("/hc", new HealthCheckOptions()
+{
+    Predicate = _ => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 app.Run();
---
+++ src/Services/Catalog/Catalog.Api/ServiceRegister.cs
@@ -0,0 +1,34 @@
+﻿using Catalog.Api.Data;
+using Catalog.Api.Data.Interfaces;
+using Catalog.Api.Repositories;
+using Catalog.Api.Repositories.Interfaces;
+using Catalog.Api.Settings;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Catalog.Api;
+
+public static class ServiceRegister
+{
+    public static IServiceCollection RegisterServices(this IServiceCollection services, IConfiguration config)
+    {
+        var catalogDbConfig = (CatalogDbSettings)config.GetSection("CatalogDbSettings").Get(typeof(CatalogDbSettings));
+
+        services.Configure<CatalogDbSettings>(config.GetSection("CatalogDbSettings"));
+
+        services.AddOptions();
+
+        services.AddHealthChecks()
+                    .AddMongoDb(catalogDbConfig.ConnectionString,
+                                "MongoDb Health",
+                                HealthStatus.Degraded);
+
+        services.AddTransient<ICatalogDbContext, CatalogDbContext>();
+        services.AddTransient<IProductRepository, ProductRepository>();
+
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        return services;
+    }
+}
