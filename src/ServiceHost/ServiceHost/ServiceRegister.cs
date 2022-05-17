2d65ef17a697e1983d00c4602186d28398bcf85c
Ali M <alimousavi-01@outlook.com>
2022-05-17 18:47:53 +0430

    Added Polly to ServiceHost HttpClient

--- src/ServiceHost/ServiceHost/ServiceRegister.cs
+++ src/ServiceHost/ServiceHost/ServiceRegister.cs
@@ -1,14 +1,43 @@
-﻿namespace ServiceHost;
+﻿using Polly;
+using Polly.Extensions.Http;
+
+namespace ServiceHost;
 
 public static class ServiceRegister
 {
     public static IServiceCollection RegisterServices(this IServiceCollection services, IConfiguration config)
     {
         services.AddHttpClient("ApiGateway",
-                               c => c.BaseAddress = new Uri(config["ApiSettings:GatewayAddress"]));
+                               c => c.BaseAddress = new Uri(config["ApiSettings:GatewayAddress"]))
+                .AddPolicyHandler(GetRetryPolicy())
+                .AddPolicyHandler(GetCircuitBreakerPolicy()); ;
 
         services.AddRazorPages();
 
         return services;
+    }
+
+    private static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+    {
+        return HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .WaitAndRetryAsync(
+                retryCount: 3,
+                sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(retryAttempt),
+                onRetry: (exception, retryCount, context) =>
+                {
+                    // TODO: Serilog
+                    Console.WriteLine($"Retry {retryCount} of {context.PolicyKey} at {context.OperationKey}, due to: {exception}.");
+                });
+    }
+
+    private static IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
+    {
+        return HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .CircuitBreakerAsync(
+                handledEventsAllowedBeforeBreaking: 5,
+                durationOfBreak: TimeSpan.FromSeconds(30)
+            );
     }
 }
