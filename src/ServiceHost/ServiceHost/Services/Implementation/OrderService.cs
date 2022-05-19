52252d5bc1b77934ffbf8254a0f76d2a01b256b3
Ali M <alimousavi-01@outlook.com>
2022-05-19 10:01:00 +0430

    Implemented OrderService

---
+++ src/ServiceHost/ServiceHost/Services/Implementation/OrderService.cs
@@ -0,0 +1,23 @@
+﻿using ServiceHost.Services.Interfaces;
+using ServiceHost.ViewModels.Order;
+
+namespace ServiceHost.Services.Implementation;
+
+public class OrderService : IOrderService
+{
+    #region ctor
+
+    private readonly HttpClient _client;
+
+    public OrderService(IHttpClientFactory factory)
+    {
+        _client = factory.CreateClient("ApiGateway");
+    }
+
+    #endregion
+
+    public async Task<IEnumerable<OrderViewModel>> GetOrdersByUserName(string userName)
+    {
+        return await _client.GetFromJsonAsync<List<OrderViewModel>>($"/order/{userName}");
+    }
+}
