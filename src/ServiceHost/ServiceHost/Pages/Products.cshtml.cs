452e4893eed1a632182a050999249fefcaf56ec2
Ali M <alimousavi-01@outlook.com>
2022-05-26 16:46:58 +0430

    Implemented Products Page

---
+++ src/ServiceHost/ServiceHost/Pages/Products.cshtml.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using ServiceHost.ViewModels.Product;
+
+namespace ServiceHost.Pages;
+
+public class ProductsModel : PageModel
+{
+    private readonly IProductService _productService;
+    private readonly IBasketService _basketService;
+
+    public ProductsModel(IProductService productService, IBasketService basketService)
+    {
+        _productService = productService;
+        _basketService = basketService;
+
+        Show = 10;
+    }
+
+    public IEnumerable<string> CategoryList { get; set; } = new List<string>();
+    public IEnumerable<ProductViewModel> ProductList { get; set; } = new List<ProductViewModel>();
+
+    [BindProperty(SupportsGet = true)]
+    public string SelectedCategory { get; set; }
+
+    [BindProperty(SupportsGet = true)]
+    public int Show { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(string categoryName)
+    {
+        var productList = await _productService.GetProducts();
+
+        CategoryList = productList.Select(p => p.Category).Distinct();
+
+        if (!string.IsNullOrWhiteSpace(categoryName))
+        {
+            ProductList = productList.Where(p => p.Category == categoryName).Take(Show);
+            SelectedCategory = categoryName;
+        }
+        else
+            ProductList = productList.Take(Show);
+
+        return Page();
+    }
+}
